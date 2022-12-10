180c67f36fa39c4cc92100d0b873f318411b3d47
code-lova <ebinuga@hotmail.com>
2022-12-10 03:11:48 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(d));
 	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(e));
 	printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+	return(0);
 }
