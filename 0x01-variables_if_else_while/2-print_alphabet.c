c1033870e21f55bda89bc770620775122f823b5b
code-lova <ebinuga@hotmail.com>
2022-12-11 10:39:04 -0800

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0, i < 26; i++)
 	{
-		putchar(alph[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
