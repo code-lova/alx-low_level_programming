2ebe176767f619707d3e4180af08664c511a549d
code-lova <ebinuga@hotmail.com>
2022-12-11 19:48:42 -0800

    success combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e, f;
+
+	i = 48;
+	e = 48;
+	f = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			f = 48;
+			while (f < 58)
+			{
+				if (e != i && e != f && i != f && e < i && i < f)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(f);
+					if (i == 56 && e == 55 && f == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				f++
+			}
+			i++
+		}
+		e++
+	}
+	putchar('\n');
+	return (0);
+}
