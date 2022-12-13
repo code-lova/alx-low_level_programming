10951fc358833211f5f47a84a5cd231a1102d5e3
code-lova <ebinuga@hotmail.com>
2022-12-13 15:59:56 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -  Prints the alphabet in lowercase.
+ * 
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	
+	if (i = 25; i <= 50)
+	{
+		i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
