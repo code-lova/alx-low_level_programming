ba15fcf9569cb9bbd10ac7ff0c919e8e91fe061d
code-lova <ebinuga@hotmail.com>
2022-12-14 07:32:46 -0800

    prints the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c; 
+	lomg int a1, a2, ij;
+
+	a1 = 1;
+	a2 = 2;
+	printf("%ld, %ld", a1, a2);
+	for (c = 0; c < 48; c++)
+	{
+		ij = a1 + a2;
+		printf(", %ld", ij);
+		a1 = a2;
+		a2 = ij;
+	}
+	printf('\n');
+	return (0);
+}
