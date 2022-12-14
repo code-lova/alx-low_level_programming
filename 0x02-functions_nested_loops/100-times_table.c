3af81e9d63a2f76203e51e28c2de3da3f0a7f28e
code-lova <ebinuga@hotmail.com>
2022-12-14 06:51:13 -0800

    prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,17 +27,17 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar(fm + 48);
 				}
-				else if (op <= 99)
+				else if (fm <= 99)
 				{
 					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar((fm / 10) + 48);
+					_putchar((fm % 10) + 48);
 				}
 				else
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(((fm / 100) % 10) + 48);
+					_putchar(((fm / 10) % 10) + 48);
+					_putchar((fm % 10) + 48);
 				}
 			}
 			_putchar('\n');
