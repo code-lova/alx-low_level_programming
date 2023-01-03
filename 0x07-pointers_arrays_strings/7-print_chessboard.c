acf4d9c7e55bb0a8be14cdc9fd73d81bef896ea8
code-lova <ebinuga@hotmail.com>
2023-01-03 22:33:27 +0800

    print the cheeseboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int j, k = 0;
+
+	for (j = 0; j < 64; j++)
+	{
+		if (j % 8 == 0 && j != 0)
+		{
+			k = j;
+			_putchar('\n');
+		}
+		_putchar(a[j / 8][j - k]);
+	}
+	_putchar('\n');
+}
