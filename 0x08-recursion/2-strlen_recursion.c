adf59dfcd801caefe67d41792cfc3eeff6f6f757
code-lova <ebinuga@hotmail.com>
2023-01-05 07:45:33 +0800

    return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+2-strlen_recursion.c[+] [unix] (07:59 01/01/1970)                                           3,1 All
+-- INSERT --
+
